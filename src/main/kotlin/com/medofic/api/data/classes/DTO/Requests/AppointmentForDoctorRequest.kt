37a7c9314acfd2de229e904c7a111cea812d1f45
druklol <707200lev@gmail.com>
2025-03-10 13:25:18 +0500

    Fixed padding

--- src/main/kotlin/com/medofic/api/data/classes/DTO/Requests/AppointmentForDoctorRequest.kt
+++ src/main/kotlin/com/medofic/api/data/classes/DTO/Requests/AppointmentForDoctorRequest.kt
@@ -1,5 +1,10 @@
 package com.medofic.api.data.classes.DTO.Requests
 
 import com.medofic.api.data.classes.Appointment
+import jakarta.validation.constraints.Pattern
 
-data class AppointmentForDoctorRequest(val snils:String, val appointment: Appointment)
+data class AppointmentForDoctorRequest(
+    @field:Pattern(regexp = "^\\d{3} \\d{3} \\d{3} \\d{2}$", message = "СНИЛС должен быть в формате ddd ddd ddd dd")
+    val snils: String,
+    val appointment: Appointment,
+)
