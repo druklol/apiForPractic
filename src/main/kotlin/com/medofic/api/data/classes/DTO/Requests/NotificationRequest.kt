ada2469667bb593992fc5bb7c86c4492a48ac2de
druklol <707200lev@gmail.com>
2025-03-10 13:25:32 +0500

    Fixed padding

--- src/main/kotlin/com/medofic/api/data/classes/DTO/Requests/NotificationRequest.kt
+++ src/main/kotlin/com/medofic/api/data/classes/DTO/Requests/NotificationRequest.kt
@@ -1,5 +1,10 @@
 package com.medofic.api.data.classes.DTO.Requests
 
 import com.medofic.api.data.classes.Notification
+import jakarta.validation.constraints.Pattern
 
-data class NotificationRequest(val snils: String, val notification: Notification)
+data class NotificationRequest(
+    @field:Pattern(regexp = "^\\d{3} \\d{3} \\d{3} \\d{2}$", message = "СНИЛС должен быть в формате ddd ddd ddd dd")
+    val snils: String,
+    val notification: Notification,
+)
