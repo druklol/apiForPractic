deae975e492e96b4b73ce61d5790a60e3648fd1c
druklol <707200lev@gmail.com>
2025-04-10 19:40:57 +0500

    Updated. Added methods for chat

--- src/main/kotlin/com/medofic/api/data/classes/Repositories/UserRepository.kt
+++ src/main/kotlin/com/medofic/api/data/classes/Repositories/UserRepository.kt
@@ -1,8 +1,13 @@
 package com.medofic.api.data.classes.Repositories
 
+import com.medofic.api.data.classes.Enums.UserRole
 import com.medofic.api.data.classes.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository:JpaRepository<User, Long> {
     fun findBySnils(snils:String):User?
+    fun findByRoleAndIsOnline(role: UserRole, isOnline: Boolean): List<User>
+    fun findByRole(role:UserRole):List<User>
 }
